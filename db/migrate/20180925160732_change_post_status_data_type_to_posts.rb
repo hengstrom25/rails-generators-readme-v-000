db80008ca1c52ba58c2aee832a446685fdf35b63
Heidi Engstrom <hengstrom25@gmail.com>
2018-09-30 18:10:33 +0000

    automatically backed up by learn

--- db/migrate/20180925160732_change_post_status_data_type_to_posts.rb
+++ db/migrate/20180925160732_change_post_status_data_type_to_posts.rb
@@ -1,5 +1,8 @@
 class ChangePostStatusDataTypeToPosts < ActiveRecord::Migration
   def change
+<<<<<<< HEAD
     change_column :posts, :post_status, :string
+=======
+>>>>>>> bb23c1b003f29d7ceee1a571f585e60d5ad99457
   end
 end
